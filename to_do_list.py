5a8b41cf738517fe8dc155ebef006c8f61b8802c
Cashadi <ichascashadi@gmail.com>
2026-06-20 12:24:20 +0700

    random number

--- to_do_list.py
+++ to_do_list.py
@@ -35,10 +35,14 @@
 # x = 5
 # print(type(x))
 
-x = 1
-y = 2.8
-z = 1j
+# x = 1
+# y = 2.8
+# z = 1j
 
-print(type(x))
-print(type(y))
-print(type(z))
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+import random
+
+print(random.randrange(1, 10))
