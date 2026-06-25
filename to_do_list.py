122b9b176747bb22a1b4ac743aa9cf416a9166eb
Cashadi <ichascashadi@gmail.com>
2026-06-25 18:10:03 +0700

    new operator

--- to_do_list.py
+++ to_do_list.py
@@ -125,3 +125,14 @@ sum3 = sum2 + sum2   # 800 (400 + 400)
 print(sum1)
 print(sum2)
 print(sum3)
+
+x = 15
+y = 4
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
