261b120ce9c7018d6ec370f2edad34cc919dba05
Cashadi <ichascashadi@gmail.com>
2026-06-28 22:58:53 +0700

    logical operator

--- to_do_list.py
+++ to_do_list.py
@@ -160,12 +160,16 @@
 # print(x)
 
 
-x = 5
-y = 3
+# x = 5
+# y = 3
 
-print(x == y)
-print(x != y)
-print(x > y)
-print(x < y)
-print(x >= y)
-print(x <= y)
+# print(x == y)
+# print(x != y)
+# print(x > y)
+# print(x < y)
+# print(x >= y)
+# print(x <= y)
+
+x = 5
+
+print(not(x > 3 and x < 10))
