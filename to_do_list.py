e4155f0f9c2e777e2dec9e23e21cc67fc14cdd7e
Cashadi <ichascashadi@gmail.com>
2026-07-05 13:02:25 +0700

    list comphrension

--- to_do_list.py
+++ to_do_list.py
@@ -221,6 +221,15 @@
 # thislist.clear()
 # print(thislist)
 
-thislist = ["apple", "banana", "cherry"]
-for x in thislist:
-  print(x)
+# thislist = ["apple", "banana", "cherry"]
+# [print(x) for x in thislist]
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+
+print(newlist)
+
