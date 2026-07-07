381f131d6eabab04a2d674544fa9aece86cb228a
Cashadi <ichascashadi@gmail.com>
2026-07-08 00:16:14 +0700

    nyoba visualization

---
+++ visualization.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+# Sample data
+x = [1, 2, 3, 4, 5]  # Example x-axis values
+y = [2, 3, 5, 7, 11]  # Example y-axis values
+
+plt.scatter(x, y)
+plt.xlabel('X-axis Label')
+plt.ylabel('Y-axis Label')
+plt.title('Scatter Plot Example')
+plt.show()
