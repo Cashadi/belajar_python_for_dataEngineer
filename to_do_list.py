fa9c7287f0d88cc90a4516b65e90f790c1125edc
Cashadi <ichascashadi@gmail.com>
2026-07-07 11:20:58 +0700

    join list

--- to_do_list.py
+++ to_do_list.py
@@ -239,6 +239,12 @@
 
 # print(thislist) 
 
-thislist = ["apple", "banana", "cherry"]
-mylist = thislist.copy()
-print(mylist)
+# thislist = ["apple", "banana", "cherry"]
+# mylist = thislist.copy()
+# print(mylist)
+
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+list3 = list1 + list2
+print(list3)
