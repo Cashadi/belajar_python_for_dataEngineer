207a8ac9fc09541291a372853a27655c2913c524
Cashadi <ichascashadi@gmail.com>
2026-07-12 14:11:55 +0700

    if condition new

--- data_engineer_forpython_datacamp.py
+++ data_engineer_forpython_datacamp.py
@@ -151,3 +151,7 @@ ingredients_needed = {"tomatoes": 400}
 # Check if you have enough tomatoes for the full party
 if pantry_stock["tomatoes"] >= ingredients_needed["tomatoes"]:
     print("Enough tomatoes for the party!")
+
+# Check if you have enough for a smaller gathering
+elif pantry_stock["tomatoes"] >= 800:
+    print("Only enough tomatoes for a smaller gathering.")
