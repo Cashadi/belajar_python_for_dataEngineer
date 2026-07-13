2d660afe22344d1c971bbbb7353b235dbdd538c5
Cashadi <ichascashadi@gmail.com>
2026-07-13 23:39:24 +0700

    last for monday

--- data_engineer_forpython_datacamp.py
+++ data_engineer_forpython_datacamp.py
@@ -179,3 +179,19 @@ for qty in quantities:
     # Otherwise it's a small quantity
     else:
         print('Small quantity')
+
+        recipe = {
+    "fusilli": 500,
+    "tomatoes": 400,
+    "basil": 20,
+    "garlic": 15,
+    "olive oil": 15,
+    "salt": 7
+}
+
+# Loop through the recipe dictionary items
+for ingredient, qty in recipe.items():
+    # Calculate the scaled quantity by multiplying by 2
+    scaled_qty = qty * 2
+    
+    print(ingredient, ":", scaled_qty, "g")
