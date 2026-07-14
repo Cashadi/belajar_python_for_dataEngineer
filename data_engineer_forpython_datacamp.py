9dce2c0ec6a7b98756eec9a03fe93c0ea1d47b67
Cashadi <ichascashadi@gmail.com>
2026-07-14 23:41:17 +0700

    while loop again

--- data_engineer_forpython_datacamp.py
+++ data_engineer_forpython_datacamp.py
@@ -205,3 +205,19 @@ while guest_count < total_confirmations:
     print(guest_count, "guests so far!")
 
 print("We have", guest_count, "guests coming!")
+
+total_ingredients = 7
+ingredients_checked = 0
+
+# Set up the loop
+while ingredients_checked < total_ingredients:
+    # Increment the counter
+    ingredients_checked += 1
+    # Check if less than 4 ingredients reviewed
+    if ingredients_checked < 4:
+        print("More than half remaining")
+    # Check if 6 or fewer ingredients reviewed
+    elif ingredients_checked <= 6:
+        print("Nearly finished checking")
+    else:
+        print("All ingredients verified!")
