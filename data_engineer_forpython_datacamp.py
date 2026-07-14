4a53d5d273b10e470a5ec21e253c1690846f6298
Cashadi <ichascashadi@gmail.com>
2026-07-15 00:14:49 +0700

    average delivery

--- data_engineer_forpython_datacamp.py
+++ data_engineer_forpython_datacamp.py
@@ -236,3 +236,13 @@ total_revenue = sum(recent_orders)
 print("Smallest order:", smallest_order)
 print("Largest order:", largest_order)
 print("Total revenue:", total_revenue)
+
+delivery_times = [19, 25, 35, 40, 28, 32, 29, 31]
+
+# Find the average delivery time
+average_time = sum(delivery_times) / len(delivery_times)
+
+# Round the average delivery time to two decimal places
+average_rounded = round(average_time, 2)
+
+print("Average delivery time:", average_rounded)
