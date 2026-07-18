1f5eab3be048602c7f20e3934963707e9bccdcbc
Cashadi <ichascashadi@gmail.com>
2026-07-18 13:16:19 +0700

    avoiding errors

--- learn_function.py
+++ learn_function.py
@@ -120,3 +120,13 @@ def concat(*args):
 
 # Call the function
 print(concat("Python", "is", "great!"))
+
+def clean_text(text):
+    # Attempt to clean the text
+    try:
+        return text.replace(" ", "_").lower()
+    # Run this code if an error occurs
+    except:
+        print("The clean_text() function expects a string as an argument, please check the data type provided!")
+    
+clean_text(187)
