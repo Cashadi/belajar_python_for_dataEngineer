8be63e59b3f5edfd8c64241d37c7525b9abcb7e3
Cashadi <ichascashadi@gmail.com>
2026-07-23 13:06:04 +0700

    again

--- to_do_list.py
+++ to_do_list.py
@@ -257,13 +257,21 @@
 
 # Comment out this line so it does not run:
 
-# Create variables
-x = 5
-y = 3.14
-z = "Hello"
+#convert from int to float:
+x = float(1)
 
-# Print the data type of each variable
+#convert from float to int:
+y = int(2.8)
+
+#convert from int to complex:
+z = complex(1)
+
+print(x)
+print(y)
+print(z)
+
 print(type(x))
 print(type(y))
 print(type(z))
+
 
