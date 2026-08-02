750bfda13e7d89052cd5f579a37db3f72d5e077b
Cashadi <ichascashadi@gmail.com>
2026-08-02 13:58:34 +0700

    if else tuple

--- python_tuple.py
+++ python_tuple.py
@@ -1,2 +1,6 @@
-thistuple = tuple(("apple", "banana", "cherry"))
-print(thistuple)
+# thistuple = tuple(("apple", "banana", "cherry"))
+# print(thistuple)
+
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
