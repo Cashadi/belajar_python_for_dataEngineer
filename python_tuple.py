c7169e79da1fb705182bac16a4028f6f0ec39307
Cashadi <ichascashadi@gmail.com>
2026-08-02 14:01:00 +0700

    update tuple

--- python_tuple.py
+++ python_tuple.py
@@ -1,6 +1,14 @@
 # thistuple = tuple(("apple", "banana", "cherry"))
 # print(thistuple)
 
-thistuple = ("apple", "banana", "cherry")
-if "apple" in thistuple:
-  print("Yes, 'apple' is in the fruits tuple")
+# thistuple = ("apple", "banana", "cherry")
+# if "apple" in thistuple:
+#   print("Yes, 'apple' is in the fruits tuple")
+
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
+
