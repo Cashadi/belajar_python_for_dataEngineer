b790dc2cc316f248a7993ef97b528fbfe56e0179
Cashadi <ichascashadi@gmail.com>
2026-08-02 14:03:11 +0700

    unpack tuple

--- python_tuple.py
+++ python_tuple.py
@@ -5,10 +5,18 @@
 # if "apple" in thistuple:
 #   print("Yes, 'apple' is in the fruits tuple")
 
-x = ("apple", "banana", "cherry")
-y = list(x)
-y[1] = "kiwi"
-x = tuple(y)
+# x = ("apple", "banana", "cherry")
+# y = list(x)
+# y[1] = "kiwi"
+# x = tuple(y)
 
-print(x)
+# print(x)
+
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+
+(green, *tropic, red) = fruits
+
+print(green)
+print(tropic)
+print(red)
 
