87d72df639d95e0f4ec9ab70b5988ff647eefe7d
Cashadi <ichascashadi@gmail.com>
2026-08-19 13:10:58 +0700

    get investment data

--- get_api.py
+++ get_api.py
@@ -1,4 +1,9 @@
 import requests
 
-response = requests.get('https://bibit.id/')
+# response = requests.get('https://bibit.id/')
+# print(response.text)
+
+investment_data = {'Name': 'Robo Advisor', 'Investment Amount': 1000000, 'Risk Level': 'Moderate', 'Investment Duration': 12}
+
+response = requests.post('https://bibit.id/', data=investment_data)
 print(response.text)
