c6c7478eee2bbfc8311a2b65f9bd69677f9a5167
Cashadi <ichascashadi@gmail.com>
2026-08-22 13:40:23 +0700

    get content_type

--- response_api.py
+++ response_api.py
@@ -2,9 +2,5 @@ import requests
 
 response = requests.get('https://netflix-api-g992.onrender.com/logout')
 
-if (response.status_code == 200):
-  print('The server responded succesfully!')
-
-# Check the response status code
-elif (response.status_code == 404):
-  print('Oops, that API could not be found!')
+# Print the response content-type header
+print(response.headers['content-type'])
