4d2d77212a008714c9f9c09f31f8a4bc8dc2eb2e
Cashadi <ichascashadi@gmail.com>
2026-08-22 13:37:19 +0700

    detection api

--- response_api.py
+++ response_api.py
@@ -2,6 +2,9 @@ import requests
 
 response = requests.get('https://netflix-api-g992.onrender.com/logout')
 
-# Check the response status code
 if (response.status_code == 200):
   print('The server responded succesfully!')
+
+# Check the response status code
+elif (response.status_code == 404):
+  print('Oops, that API could not be found!')
