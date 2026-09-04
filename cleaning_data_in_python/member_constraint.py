77bba2aae9d1b0225a394b94e151833a396ff35b
Cashadi <ichascashadi@gmail.com>
2026-09-04 12:51:01 +0700

    error di push bisa nggk

--- cleaning_data_in_python/member_constraint.py
+++ cleaning_data_in_python/member_constraint.py
@@ -5,3 +5,18 @@ print(categories)
 print('Cleanliness: ', airlines['cleanliness'].unique(), "\n")
 print('Safety: ', airlines['safety'].unique(), "\n")
 print('Satisfaction: ', airlines['satisfaction'].unique(), "\n")
+
+
+
+# yang terakhir
+# Find the cleanliness category in airlines not in categories
+cat_clean = set(airlines['cleanliness']).difference(categories['cleanliness'])
+
+# Find rows with that category
+cat_clean_rows = airlines['cleanliness'].isin(cat_clean)
+
+# Print rows with inconsistent category
+print(airlines[cat_clean_rows])
+
+# Print rows with consistent categories only
+print(airlines[~cat_clean_rows])
