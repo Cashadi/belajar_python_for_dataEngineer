908f15dd6c76ab50890ae3ef1b436decf8b4c11e
Cashadi <ichascashadi@gmail.com>
2026-09-08 10:34:28 +0700

    dictionary

---
+++ challenge_1_month_python/dictionary.py
@@ -0,0 +1,14 @@
+customer = {
+    "name": "Budi",
+    "age": 25,
+    "city": "Jakarta"
+}
+
+customer["name"] = "Andi"
+print(customer["name"])
+
+# menambahkan email ke dalam dictionary
+customer["email"] = "andi@example.com"
+print(customer["email"])
+
+print(customer)
---
+++ challenge_1_month_python/nested_dictionary.py
@@ -0,0 +1,12 @@
+customers = {
+    "customer_001": {
+        "name": "Budi",
+        "age": 25
+    },
+    "customer_002": {
+        "name": "Andi",
+        "age": 30
+    }
+}
+
+print(customers["customer_001"]["name"])
