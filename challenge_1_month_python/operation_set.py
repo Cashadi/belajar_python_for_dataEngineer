4079cbb4dd5c428cdc597a6c5fd7bfcf9df12983
Cashadi <ichascashadi@gmail.com>
2026-09-08 10:13:18 +0700

    belajar operation set

---
+++ challenge_1_month_python/operation_set.py
@@ -0,0 +1,11 @@
+a = {"Jakarta", "Bandung", "Cirebon"}
+b = {"Jakarta", "Surabaya", "Bandung"}
+
+# intersection
+print(a.intersection(b))
+
+# union
+print(a.union(b))
+
+# difference
+print(a.difference(b))
