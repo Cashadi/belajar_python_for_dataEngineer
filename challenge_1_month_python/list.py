9355bb610b1641f760244cf39168e896aa16837b
Cashadi <ichascashadi@gmail.com>
2026-09-08 10:01:44 +0700

    append and remove

--- challenge_1_month_python/list.py
+++ challenge_1_month_python/list.py
@@ -2,4 +2,10 @@ numbers = [10, 20, 30, 40, 50]
 
 numbers[0] = 100
 
-print(numbers[0])
+# menambahkan angka 22 ke dalam list
+numbers.append(22)
+
+# menghapus angka 30 dari list
+numbers.remove(30)
+
+print(numbers)
